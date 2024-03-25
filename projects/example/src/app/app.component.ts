89c14817f26857f5e2ee9820b0f05cc71b7844e1
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2024-03-25 20:12:09 +0100

    15.0.0 support

--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -1,19 +1,17 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { NgxNightwind } from '../../../ngx-nightwind/src/lib/ngx-nightwind.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  private ngxNightwind = inject(NgxNightwind)
   
-  toggle = () => this.ngxNightwind.toggle()
-  light = () => this.ngxNightwind.enableLight()
-  dark = () => this.ngxNightwind.enableDark()
+  constructor(private ngxNightwind: NgxNightwind) {
+  }
+  
+  toggle = () => this.ngxNightwind.toggle();
+  light = () => this.ngxNightwind.enableLight();
+  dark = () => this.ngxNightwind.enableDark();
 }
