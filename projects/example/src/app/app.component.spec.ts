173afb44c7282949172432b2b488c2eca73fa63d
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2024-03-29 17:34:29 +0100

    13.0.0 support

--- projects/example/src/app/app.component.spec.ts
+++ projects/example/src/app/app.component.spec.ts
@@ -4,7 +4,9 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -18,6 +20,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, example');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('example app is running!');
   });
 });
--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -1,19 +1,17 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NgxNightwind } from 'ngx-nightwind'
+import { Component } from '@angular/core';
+import { NgxNightwind } from 'ngx-nightwind';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  private ngxNightwind = inject(NgxNightwind)
   
-  toggle = () => this.ngxNightwind.toggle()
-  light = () => this.ngxNightwind.enableLight()
-  dark = () => this.ngxNightwind.enableDark()
+  constructor(private ngxNightwind: NgxNightwind) {
+  }
+  
+  toggle = () => this.ngxNightwind.toggle();
+  light = () => this.ngxNightwind.enableLight();
+  dark = () => this.ngxNightwind.enableDark();
 }
